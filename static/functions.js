d8b10ef26978fc145858f73d9220e1bf5f6a76f5
William Hicks <w.hicks917@gmail.com>
2017-05-15 23:36:13 +0000

    Update functions.js

--- static/functions.js
+++ static/functions.js
@@ -1,45 +1,80 @@
+/*function loadImg(){
+  img.src=document.getElementById("input").value;
+}
+
+function fromFile(){
+  img.src=URL.createObjectURL(document.getElementById("inputFile").files[0]);
+}*/
+
+//------------------------
 
 var displayImageFromUrl = function () {
-	var value = document.getElementById('urlContent').value,
-	img = document.createElement('img');
-	img.src = value;
-	document.body.appendChild(img);
+    var value = document.getElementById('urlContent').value, text=document.getElementById('selftext').value;
+    var img=document.getElementById("img_buffer");
+    var canv=document.getElementById("canv"),ctx=canv.getContext("2d");
+    img.src=value;
+  
+    img.onload=function(){
+      //load the image into the canvas
+      canv.width=img.width;
+      canv.height=img.height
+      ctx.drawImage(img,0,0);
+
+      //draw text
+      ctx.font="50px Comic Sans MS";
+      ctx.fillStyle="red";
+      ctx.fillText(text,50,150);
+    };
+    /*div = document.getElementById('pre'),
+    html = "<img src="+value+" alt='Image Preview' id='previewImage'>";
+    var oldImage = document.getElementById('previewImage');
+    oldImage.parentNode.removeChild(oldImage);
+    div.innerHTML = html;*/
+}
+
+function displayImageFromFile(){
+  var img=document.getElementById("output")
+  img.src=URL.createObjectURL(document.getElementById("inputFile").files[0]);
 }
 
 var inputWithUrl = function () {
-	var html = [
-		'<form method="POST" class="submitContent">',
-		'<input type="text" placeholder="url" id="urlContent" class="urlInput">',
-		'<input type="text" placeholder="text overlay" id="selftext" class="urlInput">',
-		'<button type="button" onclick="displayImageFromUrl()" class="urlInput">Preview Image</button>',
-		'</form>',
-		"<button type='button' onclick='chooseInputType()'>Back</button>",
-		].join('');
-	var div = document.getElementById('main');
-	div.innerHTML = '';
-	div.innerHTML = html;
+    var html = [
+        '<form method="POST" class="submitContent">',
+        '<input type="text" placeholder="url" id="urlContent" class="urlInput">',
+        '<input type="text" placeholder="text overlay" id="selftext" class="urlInput">',
+        '<button type="button" onclick="displayImageFromUrl()" class="urlInput">Preview Image</button>',
+        '</form>',
+        "<button type='button' onclick='chooseInputType()'>Back</button>",
+        "<div id='pre' class='previewImage'>",
+        //ADDED CANVAS
+        "<canvas id='canv'></canvas>",
+        "</div>"
+        ].join('');
+    var div = document.getElementById('main');
+    div.innerHTML = '';
+    div.innerHTML = html;
 }
 
 var inputWithFile = function () {
-	var html = [
-	"<form method='POST' class='submitContent'>",
-	"<input type='file' id='inputFile'>",
-	"<input type='submit' value='submit' id='submitFromFile' onclick=''>",
-	"</form>",
-	"<button type='button' onclick='chooseInputType()'>Back</button>",
-	].join('');
-	var div = document.getElementById('main');
-	div.innerHTML = '';
-	div.innerHTML = html;
+    var html = [
+    "<input type='file' id='inputFile'>",
+    "<button id='submitFromFile' onclick='displayImageFromFile()'>submit</button>",
+    "<button type='button' onclick='chooseInputType()'>Back</button>",
+    "<img id='output'></img>"
+    ].join('');
+    var div = document.getElementById('main');
+    div.innerHTML = '';
+    div.innerHTML = html;
 }
+
 var chooseInputType = function () {
-	var html = [
-	'<p class="inputPreface">Select image from file or from url</p>',
-	'<ul class="selectInput">',
-	'<li><button type="button" onclick="inputWithFile()">From file</button></li>',
-	'<li><button type="button" onclick="inputWithUrl()">From url</button></li>',
-	'</ul>'].join('');
-	var div = document.getElementById('main');
-	div.innerHTML = '';
-	div.innerHTML = html;
+    var html = [
+    '<p class="inputPreface">Select image from file or from url</p>',
+    '<ul class="selectInput">',
+    '<li><button type="button" onclick="inputWithFile()">From file</button></li>',
+    '<li><button type="button" onclick="inputWithUrl()">From url</button></li>',
+    '</ul>'].join('');
+    var div = document.getElementById('main');
+    div.innerHTML = '';
+    div.innerHTML = html;
 }
