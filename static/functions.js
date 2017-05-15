54a0cd0e28260b75730f36a47213901fa4cbee96
William Hicks <w.hicks917@gmail.com>
2017-05-15 02:53:42 +0000

    Update functions.js

--- static/functions.js
+++ static/functions.js
@@ -5,3 +5,38 @@ var displayImageFromUrl = function () {
 	img.src = value;
 	document.body.appendChild(img);
 }
+
+var inputWithUrl = function () {
+	var html = [
+		'<form method="POST" class="submitContent">',
+		'<input type="text" placeholder="url" id="urlContent">',
+		'<input type="text" placeholder="text overlay" id="selftext">',
+		'<button type="button" onclick="displayImageFromUrl()">Preview Image</button>',
+		'</form>',
+		"<button type='button' onclick='refresh()'>Back</button>",
+		].join('');
+	var div = document.getElementById('main');
+	div.innerHTML = '';
+	div.innerHTML = html;
+}
+
+var inputWithFile = function () {
+	var html = [
+	"<form method='POST' class='submitContent'>",
+	"<input type='file' id='inputFile'>",
+	"<input type='submit' value='submit' id='submitFromFile' onclick=''>",
+	"</form>",
+	"<button type='button' onclick='refresh()'>Back</button>",
+	].join('');
+	var div = document.getElementById('main');
+	div.innerHTML = '';
+	div.innerHTML = html;
+}
+var refresh = function () {
+	var html = [
+	'<li><button type="button" onclick="inputWithFile()">From file</button></li>',
+	'<li><button type="button" onclick="inputWithUrl()">From url</button></li>'].join('');
+	var div = document.getElementById('main');
+	div.innerHTML = '';
+	div.innerHTML = html;
+}
